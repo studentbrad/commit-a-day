4f8a0d17d362eedcc60924f67dadfce0a3844f2e
studentbrad <bradleykohler96@gmail.com>
2021-06-29 11:58:04 -0400

    use COMMITS not commits

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ DIFF=$(($UPPER - $LOWER + 1))
 COMMITS=$((RANDOM % $DIFF + $LOWER))
 git fetch
 git pull
-for i in {0..$commits}
+for i in {0..$COMMITS}
 do
 	now=$(date)
 	echo $now > date.txt
 	git add .
 	git commit -m "$now"
 	git push
-	sleep 1
+	sleep 10
 done
